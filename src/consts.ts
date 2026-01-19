ae2ead18ac0f29b69e591fc495b50dde8ebdd0fd
eulerkochy <kc99.kol@gmail.com>
2026-01-20 01:31:16 +0530

    feat: update site branding and content; replace favicon, add new blog post, and remove outdated assets

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Infinity & Beyond';
+export const SITE_DESCRIPTION = 'Systems programming, C++, Rust, and more';
